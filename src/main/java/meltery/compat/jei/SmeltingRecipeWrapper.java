77a3f538e76278316e6afc660b51cd9786e1b487
Tyler Marshall <primetoxinzz@gmail.com>
2017-06-02 00:31:28 -0400

    Fix JEI

--- src/main/java/meltery/compat/jei/SmeltingRecipeWrapper.java
+++ src/main/java/meltery/compat/jei/SmeltingRecipeWrapper.java
@@ -2,22 +2,16 @@ package meltery.compat.jei;
 
 
 import com.google.common.collect.ImmutableList;
-
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
-
-import java.awt.*;
-import java.util.List;
-
-import javax.annotation.Nonnull;
-
-import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.BlankRecipeWrapper;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.smeltery.MeltingRecipe;
+
+import javax.annotation.Nonnull;
+import java.util.List;
 
 public class SmeltingRecipeWrapper extends BlankRecipeWrapper {
 
@@ -48,11 +42,6 @@ public class SmeltingRecipeWrapper extends BlankRecipeWrapper {
         ingredients.setOutputs(FluidStack.class, outputs);
     }
 
-    @SideOnly(Side.CLIENT)
-    @Override
     public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-        String tmpStr = String.valueOf(temperature) + "\u00B0";
-        int x = 80 - minecraft.fontRenderer.getStringWidth(tmpStr) / 2;
-        minecraft.fontRenderer.drawString(tmpStr, x, 10, Color.gray.getRGB());
     }
 }
